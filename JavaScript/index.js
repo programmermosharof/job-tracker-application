856916c344bed654b37be02ceb313b50f75ef0b3
programmermosharof <mosharof.dev@gmail.com>
2026-03-21 18:33:23 +0600

    button function added and card added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,17 @@
 let currentTab = 'all';
 const tabActive = ["btn" , "btn-primary"]
 const tabInActive = ["btn", "btn-soft"]
+const allContainer = document.getElementById('all-container');
+const interviewContainer = document.getElementById('interview-container')
+const rejectedContainer = document.getElementById('reject-container')
+
+
 const switchTab = (tab) => {
     console.log(tab);
-   
-    const tabs = ["all", "interview", "rejected"]; 
-    
+    const tabs = ["all", "interview", "rejected"];  
     for (const t of tabs) {
         const tabElement = document.getElementById("tab-" + t);
-        console.log(tabElement); 
+        
         if(t === tab){
             tabElement.classList.remove(...tabInActive);
             tabElement.classList.add(...tabActive)
@@ -17,4 +20,16 @@ const switchTab = (tab) => {
             tabElement.classList.add(...tabInActive)
         }
     }
+const pages = [allContainer, interviewContainer, rejectedContainer]
+  for(let section of pages){
+        section.classList.add('hidden')
+    }
+  if(tab === "all"){
+     allContainer.classList.remove('hidden')
+    } else if (tab === "interview"){
+        interviewContainer.classList.remove('hidden')
+    }else{
+        rejectedContainer.classList.remove('hidden')
+    }
 }
+switchTab(currentTab)
